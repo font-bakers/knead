6e3e5743b9388c2898822bfc1b3ee69355d2dfc8
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-06-05 23:22:53 -0400

    TST: add test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-python knead.py --input font --output ttx --directory ../data
+find ../data/ -type f -exec sha1sum {} \; > tmp0
+
+python knead.py --input font --output ttx --directory ../data/
+
+find ../data/ -type f -exec sha1sum {} \; > tmp1
+diff tmp0 tmp1 > /dev/null
+is_diff=$?
+if [ $is_diff -eq 1 ]
+then
+    echo "Files are different."
+    exit 1
+else
+    echo "Success!"
+fi
+
+rm tmp0 tmp1
