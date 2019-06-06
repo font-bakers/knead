e38585e8cb5acd94ae50d2dd5ea28b660f099db5
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-06-06 09:50:19 -0400

    MAINT: fetch version from init

--- setup.py
+++ setup.py
@@ -1,3 +1,33 @@
+import os
+import re
 from setuptools import setup, find_packages
 
-setup(name="Knead", version="0.0.0", packages=find_packages())
+NAME = "knead"
+AUTHOR = "The Font Bakers"
+DESCRIPTION = "A data processing library for fonts and typefaces, targeting deep learning applications."
+URL = "https://github.com/font-bakers/knead/"
+LICENSE = "MIT"
+
+
+def get_version():
+    VERSION_FILE = os.path.join(NAME, "__init__.py")
+    lines = open(VERSION_FILE, "rt").readlines()
+    version_regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    for line in lines:
+        mo = re.search(version_regex, line, re.M)
+        if mo:
+            return mo.group(1)
+    raise RuntimeError("Unable to find version in %s." % (VERSION_FILE,))
+
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        version=get_version(),
+        maintainer=AUTHOR,
+        description=DESCRIPTION,
+        license=LICENSE,
+        url=URL,
+        packages=find_packages(),
+        python_requires=">=3.5.2",
+    )
