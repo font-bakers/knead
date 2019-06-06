ffdb3fa54b758d649ddf78b39012d611683da257
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-06-05 23:34:50 -0400

    DOC: add docstrings to shell script test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# Hash all files in data/ and write it to a temporary file.
 find ../data/ -type f -exec sha1sum {} \; > tmp0
 
+# Convert font to ttx. Rehash, and if data/ differs, test fail.
 python knead.py --input font --output ttx --directory ../data/
-
 find ../data/ -type f -exec sha1sum {} \; > tmp1
 diff tmp0 tmp1 > /dev/null
 if [ $? -eq 1 ]
@@ -13,4 +14,5 @@ then
     exit 1
 fi
 
+# Remove temporary files.
 rm tmp0 tmp1
