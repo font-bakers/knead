ee346e7df772bd338045933001e1dbe0296447b2
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-06-06 11:00:17 -0400

    STY: rename variables to snake case

--- knead/preprocessing/ttx_to_dict.py
+++ knead/preprocessing/ttx_to_dict.py
@@ -8,32 +8,32 @@ flags.DEFINE_string("file", None, "input ttx filename")
 flags.mark_flag_as_required("file")
 
 
-def createPoint(leftPoint, rightPoint, EMvalue):
+def create_point(left_point, right_point, em_value):
     """
-    create a virtual on-point in-between left and right off-points.
+    Create a virtual on-point in-between left and right off-points.
 
     Parameters
     ----------
-    leftPoint : XML object of the left point
+    left_point : XML object of the left point
 
-    rightPoint: XML object of the right point
+    right_point: XML object of the right point
 
-    EMvalue: size of EM box used to normalize new point
+    em_value: size of EM box used to normalize new point
 
     Returns
     -------
     New XML object that is the midpoint of left and right points
     """
-    leftPointx = int(leftPoint.get("x")) / EMvalue
-    rightPointx = int(rightPoint.get("x")) / EMvalue
-    newPointx = round(leftPointx + (rightPointx - leftPointx) / 2, 4)
-    leftPointy = int(leftPoint.get("y")) / EMvalue
-    rightPointy = int(rightPoint.get("y")) / EMvalue
-    newPointy = round(leftPointy + (rightPointy - leftPointy) / 2, 4)
-    return cElementTree.Element("pt", x=newPointx, y=newPointy, on="1")
+    left_pointx = int(left_point.get("x")) / em_value
+    right_pointx = int(right_point.get("x")) / em_value
+    new_pointx = round(left_pointx + (right_pointx - left_pointx) / 2, 4)
+    left_pointy = int(left_point.get("y")) / em_value
+    right_pointy = int(right_point.get("y")) / em_value
+    new_pointy = round(left_pointy + (right_pointy - left_pointy) / 2, 4)
+    return cElementTree.Element("pt", x=new_pointx, y=new_pointy, on="1")
 
 
-def getCurves(contour, EMvalue):
+def get_curves(contour, em_value):
     """
     unpack a contour to all of its Bezier curves
 
@@ -41,7 +41,7 @@ def getCurves(contour, EMvalue):
     ----------
     contour: XML object of the contour
 
-    EMvalue : size of EM box, used to normalize points
+    em_value : size of EM box, used to normalize points
 
     Returns
     -------
@@ -62,75 +62,75 @@ def getCurves(contour, EMvalue):
             else:
                 # create a point in between
                 points.append(last_point)
-                points = [createPoint(points[0], last_point, EMvalue)] + points
+                points = [create_point(points[0], last_point, em_value)] + points
         # now iterate through all the points and build the curves
         loc = 0
-        virtualPT = None
+        virtual_point = None
         while loc < len(points) - 2:
-            currentPT = points[loc]
-            nextPT = points[loc + 1]
-            twoNextPT = points[loc + 2]
-            startx = int(currentPT.get("x")) / EMvalue
-            starty = int(currentPT.get("y")) / EMvalue
-            nextx = int(nextPT.get("x")) / EMvalue
-            nexty = int(nextPT.get("y")) / EMvalue
-            lastx = round(int(twoNextPT.get("x")) / EMvalue, 4)
-            lasty = round(int(twoNextPT.get("y")) / EMvalue, 4)
+            current_point = points[loc]
+            next_point = points[loc + 1]
+            next_next_point = points[loc + 2]
+            startx = int(current_point.get("x")) / em_value
+            starty = int(current_point.get("y")) / em_value
+            nextx = int(next_point.get("x")) / em_value
+            nexty = int(next_point.get("y")) / em_value
+            lastx = round(int(next_next_point.get("x")) / em_value, 4)
+            lasty = round(int(next_next_point.get("y")) / em_value, 4)
             midx = round(startx + (nextx - startx) / 2, 4)
             midy = round(starty + (nexty - starty) / 2, 4)
             startx = round(startx, 4)
             starty = round(starty, 4)
             nextx = round(nextx, 4)
             nexty = round(nexty, 4)
-            if virtualPT != None:
-                startx = virtualPT.get("x")
-                starty = virtualPT.get("y")
-                currentPT = virtualPT
-                virtualPT = None
-            if currentPT.get("on") == nextPT.get("on") == "1":
+            if virtual_point != None:
+                startx = virtual_point.get("x")
+                starty = virtual_point.get("y")
+                current_point = virtual_point
+                virtual_point = None
+            if current_point.get("on") == next_point.get("on") == "1":
                 curves.append([(startx, starty), (midx, midy), (nextx, nexty)])
                 loc += 1
             elif (
-                currentPT.get("on") == "1"
-                and nextPT.get("on") == "0"
-                and twoNextPT.get("on") == "1"
+                current_point.get("on") == "1"
+                and next_point.get("on") == "0"
+                and next_next_point.get("on") == "1"
             ):
                 curves.append([(startx, starty), (nextx, nexty), (lastx, lasty)])
                 loc += 2
             elif (
-                currentPT.get("on") == "1"
-                and nextPT.get("on") == twoNextPT.get("on") == "0"
+                current_point.get("on") == "1"
+                and next_point.get("on") == next_next_point.get("on") == "0"
             ):
-                virtualPT = createPoint(nextPT, twoNextPT, EMvalue)
+                virtual_point = create_point(next_point, next_next_point, em_value)
                 curves.append(
                     [
                         (startx, starty),
                         (nextx, nexty),
-                        (virtualPT.get("x"), virtualPT.get("y")),
+                        (virtual_point.get("x"), virtual_point.get("y")),
                     ]
                 )
                 loc += 1
             else:
                 raise Exception(
                     "unhandled case",
-                    currentPT.get("on"),
-                    nextPT.get("on"),
-                    twoNextPT.get("on"),
-                    virtualPT,
-                    currentPT,
+                    current_point.get("on"),
+                    next_point.get("on"),
+                    next_next_point.get("on"),
+                    virtual_point,
+                    current_point,
                 )
     # deal with end cases
     last = points[-1]
     first = points[0]
-    startx = int(first.get("x")) / EMvalue
-    starty = int(first.get("y")) / EMvalue
-    lastx = int(last.get("x")) / EMvalue
-    lasty = int(last.get("y")) / EMvalue
+    startx = int(first.get("x")) / em_value
+    starty = int(first.get("y")) / em_value
+    lastx = int(last.get("x")) / em_value
+    lasty = int(last.get("y")) / em_value
     if len(points) > 2:
-        secondLast = points[-2]
-        secondx = int(secondLast.get("x")) / EMvalue
-        secondy = int(secondLast.get("y")) / EMvalue
-        if secondLast.get("on") == last.get("on") == "1":
+        second_last = points[-2]
+        secondx = int(second_last.get("x")) / em_value
+        secondy = int(second_last.get("y")) / em_value
+        if second_last.get("on") == last.get("on") == "1":
             midx = round(secondx + (lastx - secondx) / 2, 4)
             midy = round(secondy + (lasty - secondy) / 2, 4)
             secondx = round(secondx, 4)
@@ -139,7 +139,7 @@ def getCurves(contour, EMvalue):
             lasty = round(lasty, 4)
             curves.append([(secondx, secondy), (midx, midy), (lastx, lasty)])
         elif (
-            secondLast.get("on") == "1"
+            second_last.get("on") == "1"
             and last.get("on") == "0"
             and first.get("on") == "1"
         ):
@@ -150,15 +150,15 @@ def getCurves(contour, EMvalue):
             lastx = round(lastx, 4)
             lasty = round(lasty, 4)
             curves.append([(secondx, secondy), (lastx, lasty), (startx, starty)])
-        elif secondLast.get("on") == last.get("on") == "0" and first.get("on") == "1":
-            virtualPT = createPoint(secondLast, last, EMvalue)
+        elif second_last.get("on") == last.get("on") == "0" and first.get("on") == "1":
+            virtual_point = create_point(second_last, last, em_value)
             startx = round(startx, 4)
             starty = round(starty, 4)
             lastx = round(lastx, 4)
             lasty = round(lasty, 4)
             curves.append(
                 [
-                    (virtualPT.get("x"), virtualPT.get("y")),
+                    (virtual_point.get("x"), virtual_point.get("y")),
                     (lastx, lasty),
                     (startx, starty),
                 ]
@@ -182,7 +182,7 @@ def getCurves(contour, EMvalue):
     return curves
 
 
-def getCompContour(component, fontFile):
+def get_component_contours(component, font_file):
     """
     helper function to gather contours for a glyph that is defined in terms of
     another glyph
@@ -192,19 +192,19 @@ def getCompContour(component, fontFile):
     component: the XML object for the component tag which corresponds to a
     different glyph
 
-    fontFile: XML object of the ttx file for the given font
+    font_file: XML object of the ttx file for the given font
 
     Returns
     -------
     all the contours from the component glyph
     """
 
-    glyphName = component.get("glyphName")
-    glyph = fontFile.findall('.//TTGlyph[@name="{}"]'.format(glyphName))[0]
+    glyph_name = component.get("glyph_name")
+    glyph = font_file.findall('.//TTGlyph[@name="{}"]'.format(glyph_name))[0]
     return glyph.findall("contour")
 
 
-def getContours(glyph, glyphName, curves, fontFile, EMvalue):
+def get_contours(glyph, glyph_name, curves, font_file, em_value):
     """
     Given a glyph get all the Bezier curves from each contour
 
@@ -212,13 +212,13 @@ def getContours(glyph, glyphName, curves, fontFile, EMvalue):
     ----------
     glyph: XML object for glyph
 
-    glyphName: a string that corresponds to the name of the glyph
+    glyph_name: a string that corresponds to the name of the glyph
 
     curves: a dictionary in which all of the Bezier curves will be placed
 
-    fontFile: XML object of the fontfile, used to pass into getCompContour
+    font_file: XML object of the fontfile, used to pass into get_component_contours
 
-    EMvalue: size of EM box used for nomalization purposes
+    em_value: size of EM box used for nomalization purposes
 
     Returns
     -------
@@ -228,27 +228,27 @@ def getContours(glyph, glyphName, curves, fontFile, EMvalue):
     if not contours:
         components = glyph.findall("component")
         for component in components:
-            contours = contours + getCompContour(component, fontFile)
-    contourDict = {}
+            contours = contours + get_component_contours(component, font_file)
+    contour_dict = {}
     for i, contour in enumerate(contours):
-        c = getCurves(contour, EMvalue)
-        contourDict[i] = c
-    curves[glyphName] = contourDict
+        curves = get_curves(contour, em_value)
+        contour_dict[i] = curves
+    curves[glyph_name] = contour_dict
 
 
 def main(argv):
-    fileName = FLAGS.file
-    fileObj = open(fileName, "r")
-    fileString = fileObj.read().encode("ascii", "ignore")
-    fontFile = cElementTree.fromstring(fileString)
-    unitEM = fontFile.findall(".//unitsPerEm")[0]
-    EMvalue = float(unitEM.get("value"))
+    file_name = FLAGS.file
+    file_obj = open(file_name, "r")
+    file_string = file_obj.read().encode("ascii", "ignore")
+    font_file = cElementTree.fromstring(file_string)
+    units_per_em = font_file.findall(".//unitsPerEm")[0]
+    em_value = float(units_per_em.get("value"))
     curves = {}
-    glyphs = fontFile.findall(".//TTGlyph")
+    glyphs = font_file.findall(".//TTGlyph")
     for glyph in glyphs:
-        glyphName = glyph.get("name")
-        if glyphName in CHARACTER_SET:
-            getContours(glyph, glyphName, curves, fontFile, EMvalue)
+        glyph_name = glyph.get("name")
+        if glyph_name in CHARACTER_SET:
+            get_contours(glyph, glyph_name, curves, font_file, em_value)
 
     print("{")
     for curve, points in curves.items():
