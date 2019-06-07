ab676c6822fdd2666aa85e067b44673dc6a2636b
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-06-07 11:56:11 -0400

    TST: echo hashes

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,10 +3,12 @@ set -e
 
 # Hash all files in data/ and write it to a temporary file.
 HASH0=$(find ../data/ -type f -exec shasum {} \;)
+echo $HASH0
 
 # Convert ttf to ttx. Rehash, and if hashes differ, fail test.
 python main.py --input ttf --output ttx --directory ../data/
 HASH1=$(find ../data/ -type f -exec shasum {} \;)
+echo $HASH1
 if [ "$HASH0" != "$HASH1" ]
 then
     echo "Test failed from ttf to ttx."
@@ -18,6 +20,7 @@ fi
 # Convert ttx to json. Rehash, and if hashes differ, fail test.
 python main.py --input ttx --output json --directory ../data/
 HASH2=$(find ../data/ -type f -exec shasum {} \;)
+echo $HASH2
 if [ "$HASH0" != "$HASH2" ]
 then
     echo "Test failed from ttx to json."
