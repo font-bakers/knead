4caade0d9d328bd7ebace7b26cfaf50b591d547f
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-06-07 08:34:16 -0400

    TST: add tests for ttx2json

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,19 +4,27 @@ set -e
 # Hash all files in data/ and write it to a temporary file.
 find ../data/ -type f -exec shasum {} \; > tmp0
 
-# Convert font to ttx. Rehash, and if data/ differs, test fail.
-python main.py --input font --output ttx --directory ../data/
+# Convert ttf to ttx. Rehash, and if data/ differs, test fail.
+python main.py --input ttf --output ttx --directory ../data/
 find ../data/ -type f -exec shasum {} \; > tmp1
 diff tmp0 tmp1 > /dev/null
 if [ $? -eq 1 ]
 then
-    echo "Test failed from font to ttx."
+    echo "Test failed from ttf to ttx."
     rm tmp0 tmp1
     exit 1
 fi
 
 # Convert ttx to dict. Rehash, and if data/ differs, test fail.
-python main.py --input ttx --output dict --directory ../data/
+python main.py --input ttx --output json --directory ../data/
+find ../data/ -type f -exec shasum {} \; > tmp1
+diff tmp0 tmp1 > /dev/null
+if [ $? -eq 1 ]
+then
+    echo "Test failed from ttf to ttx."
+    rm tmp0 tmp1
+    exit 1
+fi
 
 # Remove temporary files.
 rm tmp0 tmp1
