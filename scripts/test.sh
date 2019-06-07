9464b54b3be7274d4e0c371a6b54eb87ec6b768d
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-06-07 09:28:37 -0400

    TST: fix test??

--- scripts/test.sh
+++ scripts/test.sh
@@ -18,7 +18,7 @@ fi
 # Convert ttx to json. Rehash, and if hashes differ, fail test.
 python main.py --input ttx --output json --directory ../data/
 HASH2=$(find ../data/ -type f -exec shasum {} \;)
-if [ "$HASH1" != "$HASH2" ]
+if [ "$HASH0" != "$HASH2" ]
 then
     echo "Test failed from ttx to json."
     exit 1
