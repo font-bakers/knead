c8a4caaec991d40689d9b8557cb990a160d2ac72
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-06-09 16:00:00 -0400

    BLD: add logging capabilities (#33)
    * MAINT: add clean recipe
    
    * BUG: do not remove egg-info while cleaning dir
    
    * BLD: building logging capabilities
    
    * TST: fix logging and test, but where are the logfiles?
    
    * MAINT: more helpful runtimeerror message
    
    * BUG: fix foolish renaming
    
    * MAINT: configure root logger
    
    * DOC: add better docs

--- knead/preprocessing/ttf_to_ttx.py
+++ knead/preprocessing/ttf_to_ttx.py
@@ -2,4 +2,10 @@ import subprocess
 
 
 def ttf_to_ttx(file_from, file_to):
-    subprocess.call("ttx -q -o {} {}".format(file_to, file_from).split())
+    command = "ttx -q -o {} {}".format(file_to, file_from).split()
+    return_code = subprocess.call(
+        command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+    )
+
+    if return_code != 0:
+        raise RuntimeError("fonttools ttx utility failed on {}.".format(file_from))
