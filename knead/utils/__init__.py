1d801a034e3b82619eecf99907bbef4ee0277d32
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-06-12 12:02:53 -0400

    MAINT: loosen requirements, add num_samples to npy dir (#56)
    * MAINT: remove unused imports
    
    * MAINT: remove requirements versions
    
    * MAINT: remove file extensions dict
    
    * MAINT: add num_samples in npy dir
    
    * MAINT: rename test cases

--- knead/utils/__init__.py
+++ knead/utils/__init__.py
@@ -1,2 +1,18 @@
 from . import glyph_batch_pb2
-from .utils import *
+from .utils import (
+    UPPERCASES,
+    LOWERCASES,
+    NUMERALS,
+    SPECIALS,
+    CHARACTER_SET,
+    get_filenames,
+)
+
+__all__ = [
+    "UPPERCASES",
+    "LOWERCASES",
+    "NUMERALS",
+    "SPECIALS",
+    "CHARACTER_SET",
+    "get_filenames",
+]
