a755d2aeca61bc2b4a71b8c30b5f9afe1fc70f96
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-06-19 15:05:03 +0800

    BUG: fix build, update tests

--- knead/tests/test.sh
+++ knead/tests/test.sh
@@ -2,14 +2,14 @@
 set -e
 
 # Hash all files in data/ and write it to a temporary file.
-HASH0=$(find data/ -type f -exec shasum {} \;)
-echo $HASH0
+HASH0=$(find data/ -type f -exec shasum {} +)
+find data/ -type f -exec shasum {} +
 echo
 
 # Convert ttf to ttx. Rehash, and if hashes differ, fail test.
 knead --input ttf --output ttx --directory data/
-HASH1=$(find data/ -type f -exec shasum {} \;)
-echo $HASH1
+HASH1=$(find data/ -type f -exec shasum {} +)
+find data/ -type f -exec shasum {} +
 if [ "$HASH0" != "$HASH1" ]
 then
     echo "Test failed from ttf to ttx."
@@ -21,8 +21,8 @@ echo
 
 # Convert ttx to json. Rehash, and if hashes differ, fail test.
 knead --input ttx --output json --directory data/
-HASH2=$(find data/ -type f -exec shasum {} \;)
-echo $HASH2
+HASH2=$(find data/ -type f -exec shasum {} +)
+find data/ -type f -exec shasum {} +
 if [ "$HASH0" != "$HASH2" ]
 then
     echo "Test failed from ttx to json."
@@ -37,7 +37,7 @@ echo
 # and check that nothing fails.
 knead --input json --output pb --directory data/
 NUMPBS=$(ls -1 data/pb | wc -l)
-echo $NUMPBS
+ls -1 data/pb | wc -l
 if [ $NUMPBS != 70 ]
 then
     echo "Test failed from json to pb."
@@ -50,8 +50,8 @@ echo
 # Convert pb to npy. Rehash, and if hashes differ, fail test.
 knead --input pb --output npy --directory data/
 rm -rf data/pb/  # Clean up after ourselves.
-HASH3=$(find data/ -type f -exec shasum {} \;)
-echo $HASH3
+HASH3=$(find data/ -type f -exec shasum {} +)
+find data/ -type f -exec shasum {} +
 if [ "$HASH0" != "$HASH3" ]
 then
     echo "Test failed from pb to npy."
